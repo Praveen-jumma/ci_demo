7a2c34719c7b3b7bfe3b7bcb8a11c6761e3e44eb
Jumma Praveen <praveenjumma23@gmail.com>
2025-11-30 14:54:32 +0530

    Add greeting message for user 'rahul'

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -2,6 +2,8 @@ package hello;
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello from Pipeline CI!");
+    System.out.println("Hello rahul!");
+    
     System.out.println("changes in the file");
 
   }

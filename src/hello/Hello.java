0d4fe0ff944f2dbc379ba74a29f807a1eb28d69f
Jumma Praveen <praveenjumma23@gmail.com>
2025-11-30 17:07:28 +0530

    Replace 'Hello ugyuerige' with 'Hello karthik'

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,7 +3,8 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello from Pipeline CI!");
     System.out.println("Hello rahul!");
-    System.out.println("Hello ugyuerige");
+    System.out.println("Hello karthik");
+  
     
     System.out.println("changes in the file");
 
